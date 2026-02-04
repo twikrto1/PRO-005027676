05d30d18fde8acf220ee019d4801d7a5df0d8afe
twikrto1 <32513335+twikrto1@users.noreply.github.com>
2026-02-04 10:15:11 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    vcd = {
+      source = "vmware/vcd"
+      version = "~> 3.13" # oder aktuellere laut HashiCorp
+    }
+  }
+}
+
+provider "vcd" {
+  user                 = var.vcd_user
+  password             = var.vcd_password
+  auth_type            = "integrated"
+  org                  = var.vcd_org
+  url                  = var.vcd_url
+  allow_unverified_ssl = true
+}
+
+# Beispiel: Liste aller Kataloge
+data "vcd_catalogs" "all" {}
+
+output "catalogs" {
+  value = data.vcd_catalogs.all.names
+}
