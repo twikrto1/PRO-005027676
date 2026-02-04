c093bdc9938685e475044fa1484e7f8514cec9b7
twikrto1 <32513335+twikrto1@users.noreply.github.com>
2026-02-04 16:46:03 +0100

    # vapp.tf

--- vapp.tf
+++ vapp.tf
@@ -1,10 +1,10 @@
-resource "vcd_vapp" "test_vapp" {
-  name     = "tf-test-vapp"
-  org      = var.vcd_org
-  vdc      = "Tomor"
-  power_on = false
-}
+# resource "vcd_vapp" "test_vapp" {
+# name     = "tf-test-vapp"
+# org      = var.vcd_org
+#  vdc      = "Tomor"
+#  power_on = false
+# }
 
-output "vapp_name" {
-  value = vcd_vapp.test_vapp.name
-}
+# output "vapp_name" {
+#  value = vcd_vapp.test_vapp.name
+# }
